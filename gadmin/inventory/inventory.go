e6cebbfcedb0a870b76d0c11d2ed98736c708604
Mrugesh Karnik <mkarnik@redhat.com>
2018-07-17 15:17:01 +0530

    YAML inventory writing for a new cluster

--- gadmin/inventory/inventory.go
+++ gadmin/inventory/inventory.go
@@ -1,13 +1,14 @@
 package inventory
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/spf13/afero"
-	//"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v2"
 )
 
 const inventoryDir = `/inventory`
@@ -15,12 +16,12 @@ const inventoryDir = `/inventory`
 type Inventory struct {
 	Dir          string
 	fs           afero.Fs
-	ClusterNames []string
+	clusterNames []string
 }
 
-type InventoryCluster struct {
-	name      string
-	inventory *YamlInventory
+type Cluster struct {
+	Name      string
+	inventory *ClusterInventory
 }
 
 // Ansible YAML inventory structure
@@ -40,20 +41,20 @@ type InventoryCluster struct {
 //         192.168.100.72:
 //         192.168.100.73:
 
-type InventoryHosts map[string]map[string]string // "hostname":{"var1":"val"}
+type YamlHosts map[string]map[string]string // "hostname":{"var1":"val"}
 
-type YamlHosts struct { // hosts:{"hostname":{"var1":"val"}}
-	Hosts InventoryHosts `yaml:"hosts"`
+type InventoryHosts struct { // hosts:{"hostname":{"var1":"val"}}
+	Hosts YamlHosts `yaml:"hosts"`
 }
 
-type YamlHostGroup map[string]YamlHosts // "gluster":{hosts:{"hostname":{}}}
+type YamlHostGroup map[string]InventoryHosts // "gluster":{hosts:{"hostname":{}}}
 
 type YamlAll struct { // hosts:{"hostname":{"var1":"val"}},children:{"gluster":{hosts:{"hostname":{}}}}
-	Hosts  InventoryHosts `yaml:"hosts"`
-	Groups YamlHostGroup  `yaml:"children"`
+	Hosts  YamlHosts     `yaml:"hosts"`
+	Groups YamlHostGroup `yaml:"children"`
 }
 
-type YamlInventory struct { // all:hosts:{"hostname":{"var1":"val"}},children:{"gluster":{"hostname":{}}}
+type ClusterInventory struct { // all:hosts:{"hostname":{"var1":"val"}},children:{"gluster":{"hostname":{}}}
 	All YamlAll
 }
 
@@ -99,8 +100,69 @@ func initInv(fs afero.Fs, path string) (*Inventory, error) {
 }
 
 func (inv Inventory) String() string {
-	return fmt.Sprintf("Inventory at %q has %d clusters defined.\n", inv.Dir, len(inv.ClusterNames))
+	return fmt.Sprintf("Inventory at %q has %d clusters defined.\n", inv.Dir, len(inv.clusterNames))
 }
 
 // func (inv *Inventory) LoadCluster(name string) (Cluster, error) {
 // }
+
+func (inv *Inventory) NewCluster(name string, glusterHosts []string) (Cluster, error) {
+	if inv.ContainsCluster(name) {
+		return Cluster{}, errors.New(fmt.Sprintf("Cluster named %q already in the inventory.", name))
+	}
+
+	clusterInv := NewClusterInventory(name, glusterHosts)
+	yaml, err := clusterInv.toYaml()
+	if err != nil {
+		return Cluster{}, err
+	}
+
+	// Write the YAML inventory file
+	if err := inv.writeYamlFile(name, yaml); err != nil {
+		return Cluster{}, errors.New(fmt.Sprintf("Unable to write YAML inventory: %s\n", err))
+	}
+
+	inv.addClusterName(name)
+
+	return Cluster{name, clusterInv}, nil
+}
+
+func (inv *Inventory) addClusterName(name string) {
+	inv.clusterNames = append(inv.clusterNames, name)
+}
+
+func (inv *Inventory) ContainsCluster(name string) bool {
+	for _, v := range inv.clusterNames {
+		if v == name {
+			return true
+		}
+	}
+	return false
+}
+
+func (inv *Inventory) writeYamlFile(name string, data []byte) error {
+	path := inventoryDir + fmt.Sprintf("/%s.yml", name)
+	afs := &afero.Afero{Fs: inv.fs}
+	return afs.WriteFile(path, data, 0644)
+}
+
+func NewClusterInventory(name string, glusterHosts []string) *ClusterInventory {
+	// Assemble the inventory. For now, we're ignoring the variables
+	hosts := YamlHosts{}
+	vars := make(map[string]string)
+	for _, v := range glusterHosts {
+		hosts[v] = vars
+	}
+	group := YamlHostGroup{"gluster": InventoryHosts{hosts}}
+
+	return &ClusterInventory{YamlAll{hosts, group}}
+}
+
+func (inv *ClusterInventory) toYaml() ([]byte, error) {
+	yaml, err := yaml.Marshal(inv)
+	if err != nil {
+		return yaml, errors.New(fmt.Sprintf("Unable to generate YAML inventory: %s\n", err))
+	}
+
+	return yaml, err
+}
